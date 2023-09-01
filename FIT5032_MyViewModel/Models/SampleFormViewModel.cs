43a11e41a8acaba816e4e1f60c7067967c5f2d5d
96968 <96968@QIHANG_DELL>
2023-09-01 23:48:58 +1000

    Add project files.

---
+++ FIT5032_MyViewModel/Models/SampleFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FIT5032_MyViewModel.Models
+{
+    public class SampleFormViewModel
+    {
+
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
